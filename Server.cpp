9fe0992532d04f2b5473151b0439ebb6eb18877c
Shivanshu1002 <146117856+Shivanshu1002@users.noreply.github.com>
2023-09-29 16:11:49 +0530

    make seperate class for acceptor

--- Server.cpp
+++ Server.cpp
@@ -1,19 +1,37 @@
-
 #include <iostream>
 #include <boost/asio.hpp>
 #include <boost/property_tree/ptree.hpp>
 #include <boost/property_tree/json_parser.hpp>
 #include <boost/bind/bind.hpp>
+#include <thread>
 
 using namespace std;
 using namespace boost::asio;
 using ip::tcp;
 namespace pt = boost::property_tree;
 
-void HandleClient(tcp::socket socket) {
-try{
-    
+class Acceptor {
+private:
+    tcp::acceptor acceptor_;
+public:
+    Acceptor(io_service& service) : acceptor_(service, tcp::endpoint(tcp::v4(), 12345)) {}
 
+    void AcceptConnections() {
+        cout << "Server started and waiting for connections..." << endl;
+        while (true) {
+            tcp::socket socket(acceptor_.get_io_service());
+            acceptor_.accept(socket);
+
+            // Start a new thread to handle the client
+            thread(HandleClient, std::move(socket)).detach();
+        }
+    }
+
+
+};
+
+void HandleClient(tcp::socket socket) {
+    try {
         cout << "Client connected: " << socket.remote_endpoint() << endl;
 
         // Receive data from the client
@@ -31,46 +49,25 @@ try{
         string cpuUsage = received_tree.get<string>("cpu_usage");
         string ramUsage = received_tree.get<string>("ram_usage");
         string modelName = received_tree.get<string>("model_name");
-        
-        cout << "Received CPU Usage: " << hostname<< endl;
+
         cout << "Received CPU Usage: " << cpuUsage << endl;
         cout << "Received RAM Usage: " << ramUsage << endl;
         cout << "Received Model Name: " << modelName << endl;
-         cout << "Client disconnected: " << socket.remote_endpoint() << endl;
-       socket.close();
-        
-      
-
-}
-catch (const exception& e) {
+        cout << "Client disconnected: " << socket.remote_endpoint() << endl;
+        socket.close();
+    } catch (const exception& e) {
         cerr << "Exception: " << e.what() << endl;
     }
-
 }
-
 
 int main() {
     try {
         io_service service;
-        tcp::acceptor acceptor(service, tcp::endpoint(tcp::v4(), 12345));
-        tcp::socket socket(service);
-
-
-        cout << "Server started and waiting for connections..." << endl;
-        while (1) {
-            tcp::socket socket(service);
-            acceptor.accept(socket);
-
-            // Start a new thread to handle the client
-            thread(HandleClient, std::move(socket)).detach();
-        }
-       
-       
-        
+        Acceptor acceptor(service);
+        acceptor.AcceptConnections();
     } catch (const exception& e) {
         cerr << "Exception: " << e.what() << endl;
     }
-  
 
     return 0;
 }
